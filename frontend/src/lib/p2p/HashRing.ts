5764d9171555434c8fb523e92a66fa476eaae84a
Tomás Palma <up202108880@edu.fe.up.pt>
2024-11-12 12:01:41 +0000

    add: hashring class and the ability to connect to a zmq socket in the client

--- frontend/src/lib/p2p/HashRing.ts
+++ frontend/src/lib/p2p/HashRing.ts
@@ -4,6 +4,16 @@ import { NodeIdentifier } from "./NodeIdentifier";
  * A hash ring implementation where we can compute which node to contact.
  */
 export class HashRing {
-  private ring: Map<string, NodeIdentifier> = new Map();
+    private ring: Map<string, NodeIdentifier>;
+    private starterNodes: NodeIdentifier[];
 
+    constructor() {
+        this.ring = new Map();
+
+        this.starterNodes = [
+            new NodeIdentifier(100001, "localhost", 4321, true),
+            new NodeIdentifier(100002, "localhost", 4322, true),
+            new NodeIdentifier(100003, "localhost", 4323, true),
+        ];
+    }
 }
